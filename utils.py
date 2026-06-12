e0392ad88208d9384f49b722c08724c2f8874baf
wassannc <balaram@wassan.org>
2026-06-12 16:36:42 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,8 +8,8 @@ PASSWORD = st.secrets["PASSWORD"]
 PROJECT_ID = st.secrets["PROJECT_ID"]
 
 @st.cache_data(ttl=300)
-def load_odk_data(form_id):
-    url = f"{ODK_URL}/v1/projects/{PROJECT_ID}/forms/{form_id}/Submissions.csv"
+def load_data(form_id):
+    url = f"{ODK_URL}/v1/projects/{PROJECT_ID}/forms/{form_id}/submissions.csv"
     
     response = requests.get(url, auth=(USERNAME, PASSWORD))
 
@@ -17,27 +17,12 @@ def load_odk_data(form_id):
         st.error(f"Error: {response.status_code}")
         return pd.DataFrame()
 
-    data = response.json()
-
-    if "value" not in data:
-        return pd.DataFrame()
-
-    df = pd.json_normalize(data["value"])
-    return df
-@st.cache_data(ttl=300)
-def load_entities(fisheries_2025_26):
-
-    url = f"{ODK_URL}/v1/projects/{PROJECT_ID}/datasets/{entity_name}/entities"
-
-    response = requests.get(url, auth=(USERNAME, PASSWORD))
+    import io
 
     if response.status_code != 200:
-        st.error(f"Entity Error: {response.status_code}")
+        st.error(f"Error: {response.status_code}")
         return pd.DataFrame()
 
-    data = response.json()
+    df = pd.read_csv(io.StringIO(response.text))
 
-    # Extract entity data
-    records = [item["data"] for item in data]
-
-    return pd.DataFrame(records)
+    return df
