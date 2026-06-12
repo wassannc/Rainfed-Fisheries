4e170d68fa5f00e5980df2d3e2acc1637c852ae3
wassannc <balaram@wassan.org>
2026-06-12 16:52:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,17 +11,17 @@ FORMS = {
     },
     "3. Feeding": {
         "form_id": "3. Feeding",
-        "columns": ["pd.date","pd.district","pd.block","pd.panchayat","pd.village","pd.fish_farmer","feeding.date_feeding","feeding.feed_applied","feeding.feed_source","feeding.cow_manure","feeding.poultry_manure","feeding.fishies_goat_sheep_manure","feeding.vegetables_kgs","feeding.rice_bran_kgs","feeding.oil_cake_kgs","feeding.jeevamrutham_litrs","feeding.feed_others"],
-        "district_col": "pd.district"
+        "columns": ["pd-date","pd-district","pd-block","pd-panchayat","pd-village","pd-fish_farmer","feeding-date_feeding","feeding-feed_applied","feeding-feed_source","feeding-cow_manure","feeding-poultry_manure","feeding-fishies_goat_sheep_manure","feeding-vegetables_kgs","feeding-rice_bran_kgs","feeding-oil_cake_kgs","feeding-jeevamrutham_litrs","feeding-feed_others"],
+        "district_col": "pd-district"
     },
     "4. Trailnet": {
         "form_id": "4. Trailnet",
-        "columns": ["pd.date","pd.district","pd.block","pd.panchayat","pd.village","pd.fish_farmer","trailnet.trailnet_date","trailnet.species_trailnet","trailnet.Catla_length_mm","trailnet.Ragandi_length_mm","trailnet.Mosu_length_mm","trailnet.Bangaru_papa_length_mm","trailnet.Grasscarp_length_mm","trailnet.Others_name","trailnet.Others_length_mm","trailnet.Catla_weight_grams","trailnet.Ragandi_weight_grams","trailnet.Mosu_weight_grams","trailnet.Bangaru_papa_weight_grams","trailnet.Grasscarp_weight_grams"],
-        "district_col": "pd.district"
+        "columns": ["pd-date","pd-district","pd-block","pd-panchayat","pd-village","pd-fish_farmer","trailnet-trailnet_date","trailnet-species_trailnet","trailnet-Catla_length_mm","trailnet-Ragandi_length_mm","trailnet-Mosu_length_mm","trailnet-Bangaru_papa_length_mm","trailnet-Grasscarp_length_mm","trailnet-Others_name","trailnet-Others_length_mm","trailnet-Catla_weight_grams","trailnet-Ragandi_weight_grams","trailnet-Mosu_weight_grams","trailnet-Bangaru_papa_weight_grams","trailnet-Grasscarp_weight_grams"],
+        "district_col": "pd-district"
     },
     "5. Harvesting": {
         "form_id": "5. Harvesting",
-        "columns": ["pd.date","pd.district","pd.block","pd.panchayat","pd.village","pd.fish_farmer","harvest.harvest_date","harvest.fish_avg_weight_kgs","harvest.fish_feed_expenses_rs","harvest.fish_sold_kgs","harvest.fish_solf_rate_per_kg","harvest.fish_sold_income","harvest.fish_own_consumption","harvest.fish_distributed_relatives"],
-        "district_col": "pd.district"
+        "columns": ["pd-date","pd-district","pd-block","pd-panchayat","pd-village","pd-fish_farmer","harvest-harvest_date","harvest-fish_avg_weight_kgs","harvest-fish_feed_expenses_rs","harvest-fish_sold_kgs","harvest-fish_solf_rate_per_kg","harvest-fish_sold_income","harvest-fish_own_consumption","harvest-fish_distributed_relatives"],
+        "district_col": "pd-district"
     }
 }
