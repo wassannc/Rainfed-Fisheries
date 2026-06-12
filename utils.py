77b44a6d5420cb53245ee28890d0cdd52fdff64b
wassannc <balaram@wassan.org>
2026-06-12 16:37:18 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ PASSWORD = st.secrets["PASSWORD"]
 PROJECT_ID = st.secrets["PROJECT_ID"]
 
 @st.cache_data(ttl=300)
-def load_data(form_id):
+def load_odk_data(form_id):
     url = f"{ODK_URL}/v1/projects/{PROJECT_ID}/forms/{form_id}/submissions.csv"
     
     response = requests.get(url, auth=(USERNAME, PASSWORD))
